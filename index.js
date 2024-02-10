24ab3de8b68e0c71b929bf3627f7f7951cfd3a1a
AlizeeBrunet <alizeebrunet@gmail.com>
2024-02-10 15:02:22 +0100

    entrance road

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ app.use(charactersRoutes);
 const comicsRoutes = require("./routes/comics");
 app.use(comicsRoutes);
 
+app.get("/", (req, res) => {
+  console.log("route/");
+  res.status(200).json(data);
+});
+
+app.all("*", (req, res) => {
+  console.log("route not found");
+  res.status(404).json({ message: "Not Found" });
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server has started");
 });
